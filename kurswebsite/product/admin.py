8bbbce5d99cd15e494636a84de958de43b83706a
Cesur Özgan <cesraen@gmail.com>
2023-11-04 16:20:30 +0300

    admin panelde bazı işlemler

--- kurswebsite/product/admin.py
+++ kurswebsite/product/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
+from product.models import Category
 # Register your models here.
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['title', 'status']
+    list_filter = ['status']
+    #fields = ['status]
+
+admin.site.register(Category, CategoryAdmin)
