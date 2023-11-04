3ad99ba502897b9de0fa29957a870fbf993ba1d3
Cesur Özgan <cesraen@gmail.com>
2023-11-04 11:44:09 +0300

    gerekli bağlamalar yapıldı home app açılıp template ila bağlanıldı

--- kurswebsite/home/views.py
+++ kurswebsite/home/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+
+def index(request):
+    isim = "Cesur"
+    context = {"isim": isim}
+    return render(request, 'index.html', context)
+    #return HttpResponse("Deneme")
--- kurswebsite/kurswebsite/settings.py
+++ kurswebsite/kurswebsite/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    #hoca burada başka şehil home ladı on uyaz cesur dikkat
+    'home',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
