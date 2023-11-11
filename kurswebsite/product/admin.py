7a79933c37213757cd8ba75c0432dc9323b3b001
Cesur Özgan <cesraen@gmail.com>
2023-11-11 15:34:04 +0300

    admin paneli vs hocaya göre düzeltildi benzetildi.

--- kurswebsite/product/admin.py
+++ kurswebsite/product/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from product.models import Category
+from product.models import Category, Product
 # Register your models here.
 
 class CategoryAdmin(admin.ModelAdmin):
@@ -8,3 +8,10 @@ class CategoryAdmin(admin.ModelAdmin):
     #fields = ['status]
 
 admin.site.register(Category, CategoryAdmin)
+
+
+class ProductAdmin(admin.ModelAdmin):
+    #fields = ['title, 'status']
+    list_display = ['title', 'status']
+    list_filter = ['status', 'category']
+admin.site.register(Product, ProductAdmin)
